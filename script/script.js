b3954b02c0aae78a8a6795584c5e20ee49a228de
Sebastian Linnet <sebastian.linnet@gmail.com>
2022-01-25 14:15:42 +0100

    jquery

---
+++ script/script.js
@@ -0,0 +1,22 @@
+//typewriter
+var i = 0;
+var txt = 'Sebastian';
+var speed = 150;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("navn").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+typeWriter()
+
+//smooth scroll
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 700);
+    return false;
+});
